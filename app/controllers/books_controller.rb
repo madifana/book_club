e63c39fc28bae13341775c8ddb384c7eb79264a1
Ken <github email address>
2017-06-25 14:22:26 -0700

    books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,39 @@
 class BooksController < ApplicationController
+  before_action :auth_user
+
+  def index
+    @books = Book.all
+    respond_to do |format|
+      format.html {render :index}
+      format.json {render json: @books}
+    end
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path
+    else
+      @message = @book.errors.full_messages
+      render 'new'
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    respond_to do |format|
+      format.html {render :show}
+      format.json {render json: @book}
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:id, :book_name)
+  end
 end

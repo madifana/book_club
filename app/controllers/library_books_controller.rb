57afaf8afd6562d91dfd5d753d8851d609cb1b9f
Ken <github email address>
2017-06-25 14:25:00 -0700

    library_books index method

--- app/controllers/library_books_controller.rb
+++ app/controllers/library_books_controller.rb
@@ -1,2 +1,9 @@
 class LibraryBooksController < ApplicationController
+  before_action :auth_user, :library_finder
+
+  def index
+    @library_id = params[:library_id].to_i
+    @library_books = LibraryBook.where(library_id: @library_id)
+  end
+
 end

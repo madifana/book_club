1573ddc39834d58de16fc1a4b49c5779196cedb0
Ken <github email address>
2017-06-24 20:02:20 -0700

    created book and comment models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
-  def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || root_path
+
+  def after_sign_in_path_for(user)
+    request.env['omniauth.origin'] || libraries_path
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :library
+  validates :content, presence: true
 end

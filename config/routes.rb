293f9d67e742269396c10d6e304ea8617df3dab2
Ken <github email address>
2017-06-24 19:27:42 -0700

    created join table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  resources :comments
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  end
